b3042cab25eb76912c806a62f74e520a5da647a7
Nathan D. Stevens <nstevens.work@gmail.com>
2018-09-01 13:28:54 -0500

    :new: Archive Feature 	modified:   index.js 	changes :	- Added book object to index.js 	modified:   src/log.js 	changes :	- Added archive function to log.js

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
-const log = require('./src/log');
-const error = require('./src/error');
-const info = require('./src/info');
+const log                           = require('./src/log');
+const error                         = require('./src/error');
+const info                          = require('./src/info');
+const fs                            = require('fs');
+
+class book{
+    constructor(file, type, header, locale="en-US"){
+        this.file = file
+        this.type = type
+        this.header = header
+        this.locale = locale
+    }
+
+    Draft() {
+        if (this.type === "TXT"){
+            var data = "This log was created with historia-log."
+        }
+        if (this.type === "CSV"){
+            var data = "Date"
+            this.header.forEach(header => {
+                data = data.concat(",",header);
+            })
+            data = data.concat(",","Message\r\n");
+        }
+        fs.open(this.file, 'w', (err, fd) => {
+            let found = false;
+            if (err) {
+              if (err.code === 'EEXIST') {
+                info.print("Logfile found!");
+                found = true;
+                info.print(fd);
+              }
+              else{
+                  throw err
+              }
+            }
+          
+            fs.writeFile(fd, data, (err) => {
+                if (err) throw err;
+            });
+            fs.close(fd, (err) => {
+                if (err) throw err;
+            })
+          });
+    }
+}
+
 
 module.exports = {
     log,
     error,
-    info
+    info,
+    book
 }
--- src/log.js
+++ src/log.js
@@ -1,5 +1,7 @@
 const HError                        = require('../errors');
 const chalk                         = require('chalk');
+const fs                            = require('fs');
+
 
 colors = [chalk.green, chalk.yellow, chalk.blue, chalk.magenta]
 
@@ -17,6 +19,45 @@ function print(prefixes, msg){
     console.log(result)
 }
 
+function Archive(Book, prefixes, msg){
+    let today = new Date();
+    let data = today.toLocaleDateString(Book.locale);
+    if (Book.header.length != prefixes.length){
+        throw new HError.InvalidPrefixError();
+    }
+
+    if (Book.type === "TXT"){
+        let result = '';
+        prefixes.forEach(prefix => {
+            result = result.concat('[', prefix, ']');
+        });
+        result = result.concat(" ", msg);
+        data.concat("\n\t", result)
+    }
+    if (Book.type === "CSV"){
+        prefixes.forEach( prefix => {
+            data = data.concat("," + prefix);
+        });
+        data = data.concat("," + msg);
+    }
+    data = data.concat("\r\n")
+    fs.open(Book.file, 'a', (err, fd) => {
+        if (err) throw err;
+        fs.appendFile(fd, data, 'utf8', (err) => {
+          fs.close(fd, (err) => {
+            if (err) throw err;
+          });
+          if (err) throw err;
+        });
+      });
+
+    return {
+        prefixes:prefixes,
+        msg:msg
+    }
+}
+
 module.exports = {
-    print
+    print,
+    Archive
 }
