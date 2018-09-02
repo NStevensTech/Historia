3557e324a9e6b736c2e7d2a1878f98d0df94c466
Nathan D. Stevens <nstevens.work@gmail.com>
2018-09-01 23:37:54 -0500

    :white_check_mark:Added errors.js to index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const log = require('./src/log');
 const error = require('./src/error');
 const info = require('./src/info');
+const errors = require('./errors');
 
 module.exports = {
     log,
     error,
-    info
+    info,
+    errors
 }
