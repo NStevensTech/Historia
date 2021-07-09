c10fb2e362735268af355f176c01d7b2bc0b4a59
Nathan Stevens <nate@stevens.host>
2021-07-09 01:32:46 -0500

    Typo

--- src/info.test.js
+++ src/info.test.js
@@ -1,7 +1,7 @@
 const info = require("info.js")
 
 describe("Logs for general information", () => {
-	it.todo("should print to console" () => {
+	it.todo("should print to console", () => {
 		
 	})
 })
